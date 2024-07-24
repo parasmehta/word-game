724e5be743a0eaca409c053551cbf344d690034a
parasmehta <pmehta.eu@googlemail.com>
2024-07-24 14:03:34 +0200

    working version

--- sr.py
+++ sr.py
@@ -1,43 +1,133 @@
 import streamlit as st
 import speech_recognition as sr
 from wonderwords import RandomWord
-from audiorecorder import audiorecorder
+import pyttsx3
 
-st.title("Speech Recognition")
 
-rw = RandomWord()
-# generate a random word
-word = rw.word()
+word_generator = RandomWord()
 
-st.subheader(f"Try to say the word: {word}")
 
-r = sr.Recognizer()
+def hear_pronunciation():
+    if 'current_word' in st.session_state:
+        print("pronouncing", st.session_state.current_word)
+        engine = pyttsx3.init()
+        engine.stop()
+        engine.say(st.session_state.current_word)
+        engine.runAndWait()
 
-st.title("Record Audio")
 
-file = st.file_uploader("Upload an audio file", type=["wav"])
-audio = None
+def generate_random_word():
+    word = word_generator.word()
+    st.session_state.current_word = word
+    print("word", word)
+    print("current_word", st.session_state.current_word)
 
-recording = audiorecorder("", "")
-if len(recording) > 0:
-    # To play audio in frontend:
-    st.audio(recording.export().read())
-    audio = r.record(recording.export())
 
-elif file:
+def get_current_word():
+    return st.session_state.current_word if 'current_word' in st.session_state else ""
+
+
+def get_transcription():
+    return st.session_state.transcription if 'transcription' in st.session_state else ""
+
+
+def listener():
+    ''' listen to the users speech to determine what the next step should be'''
+    r = sr.Recognizer()  # initialize a recognizer object
+    with sr.Microphone() as source:  # use the microphone
+        r.adjust_for_ambient_noise(source)
+        print("Speak Anything :")
+        audio = r.listen(source)  # record the audio, store in audio wav file
+        try:
+            text = r.recognize_sphinx(audio)
+            print("You said : {}".format(text))
+        except:
+            print("Sorry could not recognize what you said")
+            text = ""
+        st.session_state.transcription = text
+
+
+def transcribe_from_file(file):
+    r = sr.Recognizer()
+    audio = None
     st.audio(file, format="audio/wav")
 
     # use the audio file as the audio source
     with sr.AudioFile(file) as source:
         audio = r.record(source)  # read the entire audio file
 
-if not audio is None:
-    st.write("No audio found")
-    # recognize speech using Sphinx
-    try:
-        st.write("Sphinx thinks you said " +
-                 r.recognize_sphinx(audio, language="en-US"))
-    except sr.UnknownValueError:
-        st.write("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        st.write("Sphinx error; {0}".format(e))
+    transcription = ""
+    if audio:
+        try:
+            transcription = r.recognize_sphinx(audio)
+            st.write("Sphinx thinks you said " +
+                     transcription)
+        except sr.UnknownValueError:
+            st.write("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            st.write("Sphinx error; {0}".format(e))
+        st.session_state.transcription = transcription
+
+
+"""
+# Word speaking game 🎤
+
+Pick any English word and test if you speak it correctly.
+***
+
+"""
+col1, col2 = st.columns([1, 1], gap="medium")
+st.markdown("***")
+
+with col1:
+    """
+    **Enter a word or pick a random one**
+    """
+    word = st.text_input(label="Enter a word",
+                         placeholder="Enter a word", label_visibility="hidden")
+    if word:
+        st.session_state.current_word = word
+
+    st.button("Pick random word", on_click=generate_random_word)
+
+    if len(get_current_word()) > 0:
+        st.subheader(f"Try to say: {get_current_word()}")
+        hear_pronunciation()
+
+# st.button("Hear pronunciation", on_click=lambda: hear_pronunciation)
+with col2:
+    if len(get_current_word()) > 0:
+        """
+        **Record yourself or upload a recording**
+        """
+        file = st.file_uploader("Upload recording", type=[
+                                "wav"], label_visibility="hidden")
+
+        if file:
+            transcribe_from_file(file)
+
+        st.button("Record word", on_click=listener)
+
+current_word = get_current_word()
+transcription = get_transcription()
+
+if current_word and transcription and len(current_word) > 0 and len(transcription) > 0:
+    st.write(f"We expected: **{current_word}**")
+    st.write(f"You said: **{transcription}**")
+
+    if current_word.lower() == transcription.lower():
+        st.write("You got it!")
+    else:
+        st.write("Try again!")
+
+# recording = audiorecorder("", "")
+# if len(recording) > 0:
+#     # To play audio in frontend:
+#     file = recording.export()
+#     st.audio(file.read(), format="audio/wav")
+#     buffer = io.BytesIO()
+#     recording.export(buffer, format="wav")
+#     buffer.seek(0)
+#     with sr.AudioFile(buffer) as detection:
+#         recording_transcription = r.recognize_sphinx(r.record(detection))
+#         st.write("Sphinx thinks you said " + recording_transcription)
